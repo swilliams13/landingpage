a205ed0e3d6422576dd98beb51690b262d6693e7
Samantha S. Williams <swilliams13@gmail.com>
2013-01-31 20:34:55 -0500

    Update seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,15 +7,6 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 # Environment variables (ENV['...']) are set in the file config/application.yml.
 # See http://railsapps.github.com/rails-environment-variables.html
-puts 'SETTING UP DEFAULT USER LOGIN'
-user = User.create! :name => 'First User', :email => 'user1@example.com', :password => 'pleaseplease', :password_confirmation => 'pleaseplease'
-user.confirm!
-puts 'New user created: ' << user.name
-user2 = User.create! :name => 'Second User', :email => 'user2@example.com', :password => 'pleaseplease', :password_confirmation => 'pleaseplease'
-user2.confirm!
-puts 'New user created: ' << user2.name
-user.add_role :admin
-
 puts 'ROLES'
 YAML.load(ENV['ROLES']).each do |role|
   Role.find_or_create_by_name({ :name => role }, :without_protection => true)
@@ -25,4 +16,5 @@ puts 'DEFAULT USERS'
 user = User.find_or_create_by_email :name => ENV['ADMIN_NAME'].dup, :email => ENV['ADMIN_EMAIL'].dup, :password => ENV['ADMIN_PASSWORD'].dup, :password_confirmation => ENV['ADMIN_PASSWORD'].dup
 puts 'user: ' << user.name
 user.add_role :admin
-user.confirm!
+user.skip_confirmation!
+user.save!
