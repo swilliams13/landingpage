82fae667e0ece3e826ea17eae24b943796b9ae8e
Samantha S. Williams <swilliams13@gmail.com>
2013-02-05 00:23:26 -0500

    Update Mandrill API Key

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -73,7 +73,7 @@ RailsPrelaunchSignup::Application.configure do
     address: "smtp.mandrill.com",
     port: 587,
     user_name: ENV["MANDRILL_USERNAME"],
-    password: ENV["MANDRILL_API_KEY"]
+    password: ENV["MANDRILL_APIKEY"]
   }
 
  #config.action_mailer.smtp_settings = {
