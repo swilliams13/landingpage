d3f59561ba4b0d6df69281ec04228d4de2b4b0b9
Samantha S. Williams <swilliams13@gmail.com>
2013-02-05 00:03:52 -0500

    Switch production to Mandrill

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,22 +69,22 @@ RailsPrelaunchSignup::Application.configure do
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.default :charset => "utf-8"
 
-  #config.action_mailer.smtp_settings = {
-   # address: "smtp.mandrill.com",
-   # port: 587,
-   # user_name: ENV["MANDRILL_USERNAME"],
-   # password: ENV["MANDRILL_API_KEY"]
-  #}
-
- config.action_mailer.smtp_settings = {
-    address: "smtp.gmail.com",
+  config.action_mailer.smtp_settings = {
+    address: "smtp.mandrill.com",
     port: 587,
-    domain: "airfordable.co",
-    authentication: "plain",
-    enable_starttls_auto: true,
-    user_name: ENV["GMAIL_USERNAME"],
-    password: ENV["GMAIL_PASSWORD"]
+    user_name: ENV["MANDRILL_USERNAME"],
+    password: ENV["MANDRILL_API_KEY"]
   }
+
+ #config.action_mailer.smtp_settings = {
+ #   address: "smtp.gmail.com",
+ #   port: 587,
+ #   domain: "airfordable.co",
+ #   authentication: "plain",
+ #   enable_starttls_auto: true,
+ #   user_name: ENV["GMAIL_USERNAME"],
+ #   password: ENV["GMAIL_PASSWORD"]
+ # }
 
 
 
